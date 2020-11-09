5c6384465c9d86dd9d6df1ec271633b71779f3a2
Konstantin Mazur <kmazur@solvd.com>
2020-11-09 09:31:37 +0300

    Desktop container-alpha component html/css layout

--- layout/src/pug.config.js
+++ layout/src/pug.config.js
@@ -1,3 +1,10 @@
+function className(className, modifiers = []) {
+  return className + ' ' + modifiers.map(x => `${className}--${x}`).join(' ')
+}
+
 module.exports = {
   pretty: true,
+  locals: {
+    cn: className
+  }
 }
